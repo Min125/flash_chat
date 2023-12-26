e6789e5eebf7a3e128f788b5a7647aab04c6a626
myanlife-mtk <minthawkhant@myanlife.com.mm>
2023-12-26 15:27:55 +0630

    first_time

---
+++ lib/widgets/rounded_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RoundedButton extends StatelessWidget {
+  const RoundedButton(this.buttonColor,this.buttonTexts,this.onPress, {super.key});
+
+  final Color buttonColor;
+  final String buttonTexts;
+  final Function? onPress;
+
+  @override
+  Widget build(BuildContext context) {
+  return Padding(
+    padding: const EdgeInsets.symmetric(vertical: 16.0),
+    child: Material(
+      elevation: 5.0,
+      color: buttonColor,
+      borderRadius: BorderRadius.circular(30.0),
+      child: MaterialButton(
+        onPressed: () {
+          onPress!();
+        },
+        minWidth: 200.0,
+        height: 42.0,
+        child: Text(
+          buttonTexts,
+        ),
+      ),
+    ),
+  );
+  }
+}
